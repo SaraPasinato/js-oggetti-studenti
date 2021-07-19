c1dde6cc5b38c0dfadf399c63ae9928192b5ba7f
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-19 14:49:43 +0200

    add array students + write console

--- js/script_es2.js
+++ js/script_es2.js
@@ -6,4 +6,34 @@ Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
 studente inserendo nell’ordine: nome, cognome e età.
 
 */
-console.log("here");
+// initialize array of object students
+let students =[
+    {
+     name:"Giuseppe",
+     surname:"Rossi",
+     age: 30,
+    },
+    {
+        name:"Maria",
+        surname:"Verdi",
+        age: 50,
+    },
+    {
+        name:"Andrea",
+        surname:"neri",
+        age: 10,
+    },
+    {
+        name:"Marco",
+        surname:"Bianchi",
+        age: 40,
+    }
+]
+
+//debug inline
+console.table(students);
+//write students 
+for(let i=0; i< students.length;i++){
+    console.log("--------- STUDENTE "+ (i+1)+ " -----------");
+    console.log("Nome: "+ students[i].name+ "  Cognome: " + students[i].surname);
+}
