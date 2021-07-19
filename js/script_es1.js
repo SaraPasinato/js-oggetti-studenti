f954bd75d09d21fca5067cabe13f4fb85a2d841d
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-19 14:34:54 +0200

    add script_es1

--- js/script_es1.js
+++ js/script_es1.js
@@ -2,4 +2,23 @@
 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
  */
-console.log("here");
+
+//initialize by default student obj
+let student={
+ name:"John",
+ surname :"Doe",
+ age: 26,
+};
+
+const nameEl=document.getElementById("name");
+const surnameEl=document.getElementById("surname");
+const ageEl=document.getElementById("age");
+
+//write  log student
+for(var key in student){
+    console.log(key + ": "+ student[key]);
+}
+
+nameEl.innerText=student.name;
+surnameEl.innerText=student.surname;
+ageEl.innerText=student.age;
