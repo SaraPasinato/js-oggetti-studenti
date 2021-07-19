6af2038026b94c9729e9bd65809641c1d8407cc1
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-19 14:10:23 +0200

    add boilerplate

---
+++ js/script_es1.js
@@ -0,0 +1,5 @@
+/* ES 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+ */
+console.log("here");
